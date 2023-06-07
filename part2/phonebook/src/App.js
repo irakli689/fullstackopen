6c74029a6e6dac082cd9198bb34d3ca4ea9dee08
irakli689 <irakli689@gmail.com>
2023-06-07 16:52:06 +0400

    completed phonebook step6

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,15 +1,20 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
 import List from './components/list'
 import Filter from './components/filter'
 import Personform from './components/personform'
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
-  
+  const [persons, setPersons] = useState([])
+  useEffect( () => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response=>{
+        console.log('promise is fulfilled')
+        setPersons(response.data)
+      })
+  },[])
+  console.log('render', persons.length, 'notes')
   const [newName, setNewName] = useState('')
   const [namesArr, setNamesArr] = useState([...persons.map(person=>person.name)])
   const [newNumber, setNewNumber] = useState ('')
