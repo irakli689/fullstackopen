e7a80c8cd74d8a7f3b1fac7553ab35238232ef59
irakli689 <irakli689@gmail.com>
2023-08-07 06:43:41 +0400

    sdvdasa

--- part2/phonebook/src/components/filter.js
+++
@@ -1,9 +0,0 @@
-const Filter = ({value, handleChange}) => {
-    return (
-        <div>
-            filter shown with
-            <input value={value} onChange={handleChange}/>
-        </div>
-    )
-}
-export default Filter
--- part2/phonebook/src/components/list.js
+++
@@ -1,9 +0,0 @@
-const List = ({item, onDelete}) => {
-    return (
-        <li>
-            {item.name} {item.number}
-            <button onClick={onDelete}>delete</button>
-        </li>
-    )
-}
-export default List
